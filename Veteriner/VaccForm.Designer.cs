1cdaae53e732930e9df12e995a50208f95e375b1
muratapk <muratapk@hotmail.com>
2025-09-26 21:27:58 +0300

    Visits

--- Veteriner/VaccForm.Designer.cs
+++ Veteriner/VaccForm.Designer.cs
@@ -43,7 +43,15 @@
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Idtxt = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -130,6 +138,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Ekle";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -139,6 +148,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Düzeltme";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -148,6 +158,7 @@
             this.button3.TabIndex = 8;
             this.button3.Text = "Silme";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -165,6 +176,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(324, 204);
             this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // textBox2
             // 
@@ -174,11 +186,85 @@
             this.textBox2.Size = new System.Drawing.Size(200, 103);
             this.textBox2.TabIndex = 11;
             // 
+            // Idtxt
+            // 
+            this.Idtxt.Location = new System.Drawing.Point(33, 5);
+            this.Idtxt.Name = "Idtxt";
+            this.Idtxt.Size = new System.Drawing.Size(100, 20);
+            this.Idtxt.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 301);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 125);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arama";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(121, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Hayvan İsmine Göre";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(98, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Aşı İsmine Göre";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(168, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(222, 38);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Arama Yap";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(168, 20);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 41);
+            this.textBox3.TabIndex = 3;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Notlara Göre";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
             // VaccForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Idtxt);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
@@ -198,6 +284,8 @@
             this.Text = "VaccForm";
             this.Load += new System.EventHandler(this.VaccForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +308,12 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Idtxt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
--- Veteriner/VaccForm.cs
+++ Veteriner/VaccForm.cs
@@ -33,5 +33,73 @@ namespace Veteriner
         {
             dataGridView1.DataSource = baglan.vaccinations.ToList();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vaccinations vacc = new vaccinations();
+            vacc.pet_id=Convert.ToInt16(comboBox1.SelectedValue.ToString());
+            vacc.vaccine_name = textBox1.Text;
+            vacc.vaccination_date = dateTimePicker1.Value;
+            vacc.next_due_date = dateTimePicker2.Value;
+            vacc.notes = textBox2.Text;
+            baglan.vaccinations.Add(vacc);
+            baglan.SaveChanges();
+            MessageBox.Show("Kaydınız Yapılmıştır");
+            doldur();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir = dataGridView1.CurrentCell.RowIndex;
+            Idtxt.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+            comboBox1.Text= dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            textBox1.Text= dataGridView1.Rows[satir].Cells[2].Value.ToString();
+            dateTimePicker1.Text= dataGridView1.Rows[satir].Cells[3].Value.ToString();
+            dateTimePicker2.Text= dataGridView1.Rows[satir].Cells[4].Value.ToString();
+            textBox2.Text= dataGridView1.Rows[satir].Cells[5].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt16(Idtxt.Text);
+            var sonuc = baglan.vaccinations.Find(Id);
+            baglan.vaccinations.Remove(sonuc);
+            baglan.SaveChanges();
+            MessageBox.Show("Kayıt Silindi");
+            doldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt16(Idtxt.Text);
+            var sonuc = baglan.vaccinations.Find(Id);
+            sonuc.notes = textBox2.Text;
+            sonuc.vaccination_date = dateTimePicker1.Value;
+            sonuc.next_due_date = dateTimePicker2.Value;
+            sonuc.vaccine_name = textBox1.Text;
+            sonuc.pet_id = Convert.ToInt16(comboBox1.Text);
+            baglan.SaveChanges();
+            MessageBox.Show("Kayıt Güncellendi");
+            doldur();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                
+
+            }
+            else if(radioButton2.Checked)
+            {
+                var sonuc = baglan.vaccinations.Where(p => p.vaccine_name.Contains(textBox3.Text)).ToList();
+                dataGridView1.DataSource = sonuc;
+            }
+            else if(radioButton3.Checked)
+            {
+                var sonuc = baglan.vaccinations.Where(z => z.notes.Contains(textBox3.Text)).ToList();
+                dataGridView1.DataSource = sonuc;
+            }
+        }
     }
 }
